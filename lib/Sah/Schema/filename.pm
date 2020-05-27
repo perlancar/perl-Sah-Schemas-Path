16f147c277b364d992ce67fb003ed5ef104f79c8
perlancar (on pc-home) <perlancar@gmail.com>
2020-05-27 16:54:38 +0700

    Add examples (tests); forbid empty filename/dirname/pathname

--- lib/Sah/Schema/filename.pm
+++ lib/Sah/Schema/filename.pm
@@ -7,11 +7,20 @@ package Sah::Schema::filename;
 
 our $schema = [str => {
     summary => 'Filesystem file name',
+    min_len => 1,
+
     'x.completion' => ['filename'],
     prefilters => [
         'Path::expand_tilde_when_on_unix',
         'Path::strip_slashes_when_on_unix',
     ],
+
+    examples => [
+        {value=>'', valid=>0},
+        {value=>'foo', valid=>1},
+        {value=>'foo/bar', valid=>1},
+    ],
+
 }, {}];
 
 1;
--- lib/Sah/Schema/pathname.pm
+++ lib/Sah/Schema/pathname.pm
@@ -7,11 +7,19 @@ package Sah::Schema::pathname;
 
 our $schema = [str => {
     summary => 'Filesystem path name',
+    min_len => 1,
     'x.completion' => ['filename'],
     'prefilters' => [
         'Path::expand_tilde_when_on_unix',
         'Path::strip_slashes_when_on_unix',
     ],
+
+    examples => [
+        {value=>'', valid=>0},
+        {value=>'foo', valid=>1},
+        {value=>'foo/bar', valid=>1},
+    ],
+
 }, {}];
 
 1;
