2bd795b362083c2e300cf8976b3cab76f3ad94fa
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-17 18:04:19 +0700

    [doc] Give rationale for the schemas

--- lib/Sah/Schema/dirname.pm
+++ lib/Sah/Schema/dirname.pm
@@ -7,6 +7,24 @@ package Sah::Schema::dirname;
 
 our $schema = [str => {
     summary => 'Filesystem directory name',
+    description => <<'_',
+
+This schema is basically string with some checks and prefilters. Why use this
+schema instead of plain ol' str? Mainly to give you the ability to change tilde
+to user's home directory, e.g. `~/foo` into `/home/someuser/foo`. Normally this
+expansion is done by a Unix shell, but sometimes your program receives an
+unexpanded path, e.g. when you get it from some config file.
+
+See also more OS-specific schemas like `dirname::unix`, which adds some more
+checks (e.g. filename cannot contain forward slash and each component cannot be
+longer than 255 characters) and preprocessing (e.g. stripping extraneous slashes
+like `foo//bar` into `foo/bar`.
+
+What's the difference between this schema and `filename`? The default completion
+rule. This schema's completion by default only includes directories.
+
+_
+
     min_len => 1,
 
     'x.completion' => ['dirname'],
--- lib/Sah/Schema/filename.pm
+++ lib/Sah/Schema/filename.pm
@@ -7,6 +7,25 @@ package Sah::Schema::filename;
 
 our $schema = [str => {
     summary => 'Filesystem file name',
+    description => <<'_',
+
+This schema is basically string with some checks and prefilters. Why use this
+schema instead of plain ol' str? Mainly to give you the ability to change tilde
+to user's home directory, e.g. `~/foo.txt` into `/home/someuser/foo.txt`.
+Normally this expansion is done by a Unix shell, but sometimes your program
+receives an unexpanded path, e.g. when you get it from some config file.
+
+See also more OS-specific schemas like `filename::unix`, which adds some more
+checks (e.g. filename cannot contain forward slash and each component cannot be
+longer than 255 characters) and preprocessing (e.g. stripping extraneous slashes
+like `foo//bar` into `foo/bar`.
+
+What's the difference between this schema and `dirname`? The default completion
+rule. This schema's completion by default only includes files and not
+directories.
+
+_
+
     min_len => 1,
 
     'x.completion' => ['filename'],
