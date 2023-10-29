cc2f19f354ca437fea105501388224630b0c26fd
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-10-29 08:32:39 +0700

    Add schema: dirname::default_curdir

---
+++ lib/Sah/Schema/dirname/default_curdir.pm
@@ -0,0 +1,31 @@
+package Sah::Schema::dirname::default_curdir;
+
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $schema = ['dirname' => {
+    summary => 'Directory name, default to current directory',
+    description => <<'_',
+
+Note: be careful when using this schema for actions that are destructive,
+because a user can perform those actions without giving an argument (e.g. a
+`delete-files-in` script). It is safer to use this schema to perform a
+non=destructive action (e.g. `ls`) and/or operate in dry-run mode by
+default.
+
+_
+
+    default => '.',
+
+    examples => [
+    ],
+
+}];
+
+1;
+# ABSTRACT:
