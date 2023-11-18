2081cca145819affd9f73498c5b25410e2f545ec
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-18 10:49:41 +0700

    Grammar

--- lib/Sah/Schema/dirname/default_curdir.pm
+++ lib/Sah/Schema/dirname/default_curdir.pm
@@ -13,10 +13,9 @@ our $schema = ['dirname' => {
     description => <<'_',
 
 Note: be careful when using this schema for actions that are destructive,
-because a user can perform those actions without giving an argument (e.g. a
-`delete-files-in` script). It is safer to use this schema to perform a
-non=destructive action (e.g. `ls`) and/or operate in dry-run mode by
-default.
+because a user can perform those actions without giving an argument (e.g. in a
+`delete-files-in` script). It is safer to use this schema when performing a
+non=destructive action (e.g. `ls`) and/or operate in dry-run mode by default.
 
 _
 
