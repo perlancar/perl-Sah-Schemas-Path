d9d98378d47c53e52cfb0c241fc3542a251cb592
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-23 12:40:12 +0700

    Add schema: filename::default_only_file_not_dir_in_curdir

--- lib/Sah/Schema/filename/default_only_file_not_dir_in_curdir.pm
+++ lib/Sah/Schema/filename/default_only_file_not_dir_in_curdir.pm
@@ -1,4 +1,4 @@
-package Sah::Schema::filename::default_only_file_in_curdir;
+package Sah::Schema::filename::default_only_file_not_dir_in_curdir;
 
 use strict;
 
@@ -8,17 +8,16 @@ use strict;
 # VERSION
 
 our $schema = ["filename" => {
-    summary => 'File name, defaults to only file in current directory (if there is one)',
+    summary => 'File name, defaults to only file in current directory (if there is one) (subdirectories ignored)',
     description => <<'MARKDOWN',
 
 This is like the `filename` schema but with a default value of "only file in the
 current directory". That is, if the current directory has a single plain file
-and nothing else.
+and nothing else (subdirectories are ignored).
 
-Difference with `filename::default_only_file_not_subdir_in_subdir` schema: the
-other schema ignores subdirectories. Thus, if a directory only contains `file1`
-and `subdir1`, then that other schema will return `file1` but this schema will
-not return a default value.
+Difference with `filename::default_only_file_in_subdir` schema: the other schema
+does not ignore subdirectories. Thus, if a directory only contains `file1` and
+`subdir1`, then that other schema will not return `file1` but this schema will.
 
 Note: be careful when using this schema for actions that are destructive,
 because a user can perform those actions without giving an argument (e.g. in a
@@ -27,7 +26,7 @@ non=destructive action (e.g. `checksum`) and/or operate in dry-run mode by
 default.
 
 MARKDOWN
-    'x.perl.default_value_rules' => ['Path::only_file_in_curdir'],
+    'x.perl.default_value_rules' => ['Path::only_file_not_dir_in_curdir'],
 }];
 
 1;
